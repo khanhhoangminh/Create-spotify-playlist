f7462039f920bbd3f29666d5d8378d9e65e780ea
khanhhoangminh <tinhdaulangson@gmail.com>
2024-08-13 12:14:37 +0700

    Add files via upload

---
+++ Time Machine/main.py
@@ -0,0 +1,79 @@
+
+CLIENT_ID = "756da0db4d84495eb155427fc5146b5a"
+CLIENT_SECRET = "aa66ee243be24338b3c8001ed413bcb6"
+import base64
+import requests
+import json
+
+
+def get_token():
+    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = requests.post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = requests.get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists...")
+        return None
+
+    return json_result[0]
+
+def get_user_id(token):
+    url = "https://api.spotify.com/v1/me"
+    headers = get_auth_header(token)
+    response = requests.get(url=url, headers=headers)
+    return response.text
+
+token = get_token()
+print(token)
+user_id = get_user_id(token)
+print(user_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
